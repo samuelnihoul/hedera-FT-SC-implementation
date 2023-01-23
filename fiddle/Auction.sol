02f1f8f8874e1b6a37eb3fd56509cbe88a88f100
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2023-01-23 20:29:20 +0100

    the new version of Auction.sol is almost ready, I just have to finish with the division problem

--- fiddle/Auction.sol
+++ fiddle/Auction.sol
@@ -12,20 +12,20 @@ interface Token {
 }
 
 contract WeeklyAuction {
-    event Start(uint64 auctionNumber);
+    event Start(uint256 auctionNumber);
     event Bid(address indexed sender, uint256 amount);
-    event End(mapping(address => uint256) bids);
+    event End(address[] bidderList);
     Token public tokenToBeAttributed;
-    address public seller;
     uint256 public endAt;
     bool public started;
     bool public ended;
     string tokenID;
-    address[] biderList;
+    address[] bidderList;
     address harmonia_eko;
     mapping(address => uint256) public bids;
     uint256 public totalBiddedAmount;
     uint256 public auctionNumber;
+    
 
     constructor(
         address _tokenToBeAttributed,
@@ -34,20 +34,19 @@ contract WeeklyAuction {
     ) {
         tokenToBeAttributed = Token(_tokenToBeAttributed);
         tokenID = _tokenID;
-        seller = msg.sender;
-        highestBids = [0];
-        harmonia -
-            eko = _harmonia - eko;
+        
+        
+        harmonia_eko
+            = _harmonia_eko;
     }
 
-    function start() external {
+    function start() public {
         require(!started, "started");
-        require(msg.sender == seller, "not seller");
+        require(msg.sender == harmonia_eko, "not authority");
         tokenToBeAttributed.transferFrom(msg.sender, address(this), 1);
         auctionNumber += 1;
         started = true;
         endAt = block.timestamp + 30 seconds;
-
         emit Start(auctionNumber);
     }
 
@@ -56,7 +55,7 @@ contract WeeklyAuction {
         require(block.timestamp < endAt, "ended");
         bids[msg.sender] += msg.value;
         totalBiddedAmount += msg.value;
-        biderList[biderList.length + 1] = msg.sender;
+        bidderList[bidderList.length + 1] = msg.sender;
         emit Bid(msg.sender, msg.value);
     }
 
@@ -65,17 +64,17 @@ contract WeeklyAuction {
         require(block.timestamp >= endAt, "not ended");
         require(!ended, "ended");
         ended = true;
-        if (highestBidders.length != 0) {
-            for (uint64 i = 0; i < biderList.length; i++) {
+        if (bidderList.length != 0) {
+            for (uint64 i = 0; i < bidderList.length; i++) {
                 tokenToBeAttributed.safeTransferFrom(
                     address(this),
-                    biderList[i],
-                    (0.99 * bids[biderList[i]]) / totalBiddedAmount
+                    bidderList[i],
+                    bids[bidderList[i]] /totalBiddedAmount
                 );
             }
-            tokenToBeAttributed.safeTransferFrom(address.this, harmonia - eko);
+            tokenToBeAttributed.safeTransferFrom(address(this), harmonia_eko,0.01*totalBiddedAmount);
         }
-        emit End(bids);
+        emit End(bidderList);
         start();
     }
 }
