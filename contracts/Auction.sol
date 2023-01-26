dff1b152421a5f5bf4fb52c545c205a052289161
Samuel Nihoul <69475809+samuelnihoul@users.noreply.github.com>
2023-01-26 12:04:11 +0100

    contract ready to test1

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -2,17 +2,15 @@
 pragma solidity 0.8.17;
 import "../../hedera-smart-contracts/contracts/safe-hts-precompile/SafeHTS.sol";
 
-
-
 contract KarbonMoneta is SafeHTS {
     event Start(uint256 auctionNumber);
     event Bid(address indexed sender, uint256 amount);
     event End(address[] bidderList);
-    Token public token;
+    IHederaTokenService.HederaToken public token= IHederaTokenService.HederaToken(name:'Karbon Moneta',symbol:'KM',treasury:corporateAddress);
     uint256 public endAt;
     bool public started;
     bool public ended;
-    string tokenID;
+    address tokenAddress;
     address[] bidderList;
     address corporateAddress;
     mapping(address => uint256) public bids;
@@ -21,18 +19,12 @@ contract KarbonMoneta is SafeHTS {
 
     constructor() {
         corporateAddress = msg.sender;
-    }
-
-    function createToken() public {
-        IHederaTokenService.HederaToken _token= IHederaTokenService.HederaToken(name:'Karbon Moneta',symbol:'KM',treasury:corporateAddress)
-        require(msg.sender == corporateAddress, "not authority");
-        safeCreateFungibleToken(token)
+        tokenAddress=safeCreateFungibleToken(token,1100000000000000,8)
     }
 
     function start() public {
         require(!started, "started");
         require(msg.sender == corporateAddress, "not authority");
-        tokenToBeAttributed.transferFrom(msg.sender, address(this), 1);
         auctionNumber += 1;
         started = true;
         endAt = block.timestamp + 30 seconds;
@@ -53,15 +45,16 @@ contract KarbonMoneta is SafeHTS {
         require(block.timestamp >= endAt, "not ended");
         require(!ended, "ended");
         ended = true;
+        SafeHTS.safeMintToken(tokenAddress,100000000);
         if (bidderList.length != 0) {
             for (uint64 i = 0; i < bidderList.length; i++) {
-                tokenToBeAttributed.safeTransferFrom(
+                SafeHTS.safeTransferToken(
                     address(this),
                     bidderList[i],
                     bids[bidderList[i]] / totalBiddedAmount
                 );
             }
-            tokenToBeAttributed.safeTransferFrom(
+            SafeHTS.safeTransferFrom(
                 address(this),
                 corporateAddress,
                 totalBiddedAmount / 100
